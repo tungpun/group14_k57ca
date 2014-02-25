37f381dc1d7c4f288c501450223076759507e4fc
tungnh <htung.nht@gmail.com>
2014-02-25 18:23:06 +0700

    Fix merge cell

--- mytimetable/timetables/views.py
+++ mytimetable/timetables/views.py
@@ -13,10 +13,35 @@ def index(request):
 
 
 def board(request):
+    objs = []
+
     periods_array = Period.objects.filter(timetable_id=2)
     for period in periods_array:
-        period.position -= 1
-        period.day = str(period.position / 10)
-        period.per = str(period.position % 10)
-    content = {'periods_array': periods_array}
+        objs.append(period)
+
+    for i in range(0, 71):
+        free = True
+        for period in periods_array:
+            if period.position == i:
+                free = False
+                break
+        if free:
+            mark = False
+            for period in periods_array:
+                if (period.position < i) and (i < period.position + period.length):
+                    mark = True
+                    break
+            if mark:
+                objs.append(Period(position=i, name=0))
+            else:
+                objs.append(Period(position=i, name=""))
+
+    for obj in objs:
+        obj.day = str((obj.position - 1) / 10)
+        obj.per = str((obj.position - 1) % 10)
+
+    objs.sort(key=lambda obj: obj.position)
+    #objs.sort(cmp=None,key=position,request=False)
+
+    content = {'periods_array': objs}
     return render(request, 'timetables/board/index.html', content)
