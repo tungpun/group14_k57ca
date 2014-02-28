0bec7e7a0bc6c8197f51677f34cb1bb715981561
Hoang Vuong <hoangvuong94st@gmail.com>
2014-02-28 10:16:31 +0700

    update subject info

--- periods/urls.py
+++ periods/urls.py
@@ -4,6 +4,5 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    #url(r'^subjects/$', views.subjects, name='subjects'),
     url(r'^id=(?P<pid>\d+)', views.information, name='information'),
 )
