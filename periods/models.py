3fcd13d0c1970c734dc089f916bbbab9f2c1abd8
blacksteed <le_anh_vu07@yahoo.com.vn>
2014-03-09 23:56:11 +0700

    Add multiple choice box

--- periods/models.py
+++ periods/models.py
@@ -5,12 +5,50 @@ from timetables.models import Timetable
 
 #class Timetable(models.Model):
  #   pass
+<<<<<<< HEAD
+MONDAY = 0
+TUESDAY = 1
+WEDNESDAY = 2
+THURSDAY = 3
+FRIDAY = 4
+SATURDAY = 5
+SUNDAY = 6
+DAY_CHOICES = (
+    (MONDAY, 'Monday'),
+    (TUESDAY, 'Tuesday'),
+    (WEDNESDAY, 'Wednesday'),
+    (THURSDAY, 'Thursday'),
+    (FRIDAY, 'Friday'),
+    (SATURDAY, 'Saturday'),
+    (SUNDAY, 'Sunday')
+)
+
+ORDER_CHOICES = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+    (6, '6'),
+    (7, '7'),
+    (8, '8'),
+    (9, '9'),
+    (10, '10'),
+)
 
 
 class Period(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=30)
     lecturer = models.CharField(max_length=30)
+    day = models.IntegerField(max_length=2,
+                                choices=DAY_CHOICES,
+                                default=MONDAY)
+    start = models.IntegerField(max_length=2,
+                                choices=ORDER_CHOICES,
+                                default=1)
+    length = models.IntegerField(max_length=2)
+    timetable = models.ForeignKey(Timetable)
     position = models.IntegerField(max_length=2)
     length = models.IntegerField(max_length=2)
     timetable = models.ForeignKey(Timetable)
