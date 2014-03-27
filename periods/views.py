0eef2ec9078716e560a28ecfa94ea474d5a6c80e
tunglx <tunglx_57@vnu.edu.vn>
2014-03-27 14:14:02 +0700

    [periods] get other user's period

--- periods/views.py
+++ periods/views.py
@@ -32,6 +32,52 @@ def detail(request, pid="0"):
         'periods': objs,
     }
     return render(request, 'periods/detail/index.html', content)
+    
+def check_conflict2(periods, pid):
+        periods_array = Period.objects.filter(id=pid)
+        pos = periods.position
+        leg = periods.length
+        free = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+                57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73]
+        for f in free:
+            f = True
+        for period in periods_array:
+            free[period.position] = False
+            j = period.position
+            while j <= period.position + period.length-1:
+                if (j > 0) and (j < 71):
+                    free[j] = False
+                j += 1
+        j = pos
+        while j <= pos+leg-1:
+            if j > 70:
+                return False
+            if not free[j]:
+                return False
+            else:
+                free[j] = False
+            j += 1
+        return True
+
+def get(request, pid='0'):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/users/auth_login')
+    if request.method == 'GET':
+                current_user = request.user
+                periods = Period.objects.get(id=pid)
+                new_period = Period(code=periods.code,
+                                    name=periods.name,
+                                    lecturer=periods.lecturer,
+                                    position=periods.position,
+                                    length=periods.length,
+                                    timetable_id=current_user.id)
+                if check_conflict2(periods, current_user.id):
+                    new_period.save()
+                    return HttpResponseRedirect('/')
+                else:
+                    return HttpResponse("Can't get this subject")
 					
 def edit(request, pid="0"):
     if not request.user.is_authenticated():
